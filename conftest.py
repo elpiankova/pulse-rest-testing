419f84c59bae5920fa32626bcf28426e9cdf9796
elpiankova <el.piankova@gmail.com>
2020-04-22 17:00:16 +0300

    Use parametrized fixture and add 'teardown' part

--- conftest.py
+++ conftest.py
@@ -7,10 +7,20 @@ def base_url():
     return "http://pulse-rest-testing.herokuapp.com/"
 
 
-@pytest.fixture()
-def book_data():
-    data = {"title": "111", "author": "0000"}
-    return data
+data_list = [
+    {"title": "112345    ", "author": "0235657"},
+    {"title": "asdf sdfsd", "author": "hbjbsd sdfdsf"},
+    {"title": "!@#$%^&*()", "author": "{}:\";'<?'}"},
+    {"title": "ЯЧСМИваыва", "author": "ПМРМРМ ллрлрт"}
+]
+
+
+@pytest.fixture(params=data_list, ids=[str(item) for item in data_list])
+def book_data(request, base_url):
+    data = request.param
+    yield data
+    if "id" in data:
+        r = requests.delete(f"{base_url}books/{data['id']}")
 
 
 @pytest.fixture()
--- test_books_pytest.py
+++ test_books_pytest.py
@@ -2,21 +2,23 @@ import pytest
 import requests
 
 
-data_list = [
-    {"title": "112345    ", "author": "0235657"},
-    {"title": "asdf sdfsd", "author": "hbjbsd sdfdsf"},
-    {"title": "!@#$%^&*()", "author": "{}:\";'<?'}"},
-    {"title": "ЯЧСМИваыва", "author": "ПМРМРМ ллрлрт"}
-]
+# data_list = [
+#     {"title": "112345    ", "author": "0235657"},
+#     {"title": "asdf sdfsd", "author": "hbjbsd sdfdsf"},
+#     {"title": "!@#$%^&*()", "author": "{}:\";'<?'}"},
+#     {"title": "ЯЧСМИваыва", "author": "ПМРМРМ ллрлрт"}
+# ]
 
 
-@pytest.mark.parametrize("data", data_list, ids=[str(item) for item in data_list])
-def test_create_book(base_url, data):
-    r = requests.post(base_url+"books/", data=data)
+# @pytest.mark.parametrize("data", data_list, ids=[str(item) for item in data_list])
+
+
+def test_create_book(base_url, book_data):
+    r = requests.post(base_url+"books/", data=book_data)
     assert r.status_code == 201
     r_body = r.json()
-    data["id"] = r_body["id"]
-    assert r_body == data
+    book_data["id"] = r_body["id"]
+    assert r_body == book_data
 
 
 def test_update_book(base_url, init_book):
